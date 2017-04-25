5cf0d0c36b57c15a46bcd9260d8e61c024dea448
jekst <jekst@qq.com>
2017-04-25 09:51:28 +0800

    update

--- src/jquery.image-preview.js
+++ src/jquery.image-preview.js
@@ -1,7 +1,18 @@
 /**
  * 图片预览插件
  */
-(function ($) {
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node / CommonJS
+        factory(require('jquery'));
+    } else {
+        // Browser globals.
+        factory(jQuery);
+    }
+})(function ($) {
     $.fn.imagePreview = function (option) {
         var defaultOption = {
             multiple: false,
@@ -50,4 +61,4 @@
         });
         return this;
     };
-})(jQuery);
+});
